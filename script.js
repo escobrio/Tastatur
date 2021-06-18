5b838e687372dc78e02ff1449ef1705aa352db9d
escobrio <86103053+escobrio@users.noreply.github.com>
2021-06-18 14:17:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function putOut1() {
+    var userInput = document.getElementById("button1").innerHTML;
+    document.getElementById("outputBox").innerHTML = userInput;
+}
+
+function putOut2() {
+    var userInput = document.getElementById("button2").innerHTML;
+    document.getElementById("outputBox").innerHTML = userInput;
+}
+
+function putOut3() {
+    var userInput = document.getElementById("button3").innerHTML;
+    document.getElementById("outputBox").innerHTML = userInput;
+}
+
+function putOut4() {
+    var userInput = document.getElementById("button4").innerHTML;
+    document.getElementById("outputBox").innerHTML = userInput;
+}
